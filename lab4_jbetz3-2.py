c05d4aabdb6a64cdbc11ea4adb995dcb6081e39b
Thysmall <jordan@dbimages.com>
2026-02-04 15:19:24 -0500

    Initial documentation added

---
+++ lab4_jbetz3-2.py
@@ -0,0 +1,7 @@
+"""
+Name: Guess the Number, Keep the Kids
+Author: Jordan Betz
+Description: Guess the number game where as you guess incorrect your wife builds the car and takes the kids.
+Date: 2/4/26
+Credits: ASCII art from: https://www.asciiart.eu/art/5e3d4e6de976abac
+"""
