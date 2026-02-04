ed023f3aa11f382c679e6f2ac807aabea7f40541
Thysmall <jordan@dbimages.com>
2026-02-04 15:56:47 -0500

    Added gameplay including if statements to check guess relativity to secret number, win screen, and lose message.

--- lab4_jbetz3-2.py
+++ lab4_jbetz3-2.py
@@ -3,13 +3,13 @@ Name: Guess the Number, Keep the Kids
 Author: Jordan Betz
 Description: Guess the number game where as you guess incorrect your wife builds the car and takes the kids.
 Date: 2/4/26
-Credits: ASCII art from: https://www.asciiart.eu/art/5e3d4e6de976abac
+Credits: ASCII car art from: https://www.asciiart.eu/art/5e3d4e6de976abac
 """
 
 import random
 
 number: int = random.randint(1,15)
-stage: int = -1
+stage: int = 0
 art: list[str] = [
     """
     
@@ -46,6 +46,12 @@ art: list[str] = [
      /|o||0\\`.__
     (   _    _ _\\
     =`-(_)--(_)-' 
+    """,
+    """
+         \\O/   
+      \\o/ | \\o/
+       |  |  |
+       ^  ^  ^
     """    
 ]
 
@@ -56,7 +62,22 @@ while playing:
     try:
         guess: int = int(input("Enter your guess: "))
         
-        playing = False
+        if guess == number:
+            print("You guessed correct! You get to keep the kids!")
+            print(art[6])
+            playing = False
+            break
+        if guess < number:
+            print("Thats not it. (Timmy says its higher)")
+            print(art[stage])
+        if guess > number:
+            print("Thats not it. (Jimmy says its lower)")
+            print(art[stage])
+        stage += 1
+        if stage > 5:
+            print("You lost! Your wife took the kids.")
+            playing = False
+        
     except:
         if ValueError: print("You must guess an integer.")      
     
