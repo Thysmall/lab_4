eba759d5a1637bf59d99c87ee0314e32ff8a6511
Thysmall <jordan@dbimages.com>
2026-02-11 13:48:08 -0500

    Updated date to date of second submission

--- lab4_jbetz3-2.py
+++ lab4_jbetz3-2.py
@@ -3,7 +3,7 @@ Name: Guess the Number, Keep the Kids
 Author: Jordan Betz
 Description: Guess the number game where as you guess incorrect your wife 
              builds the car and takes the kids.
-Date: 2/4/26
+Date: 2/11/26
 Credits: ASCII car art from: https://www.asciiart.eu/art/5e3d4e6de976abac
 """
 
